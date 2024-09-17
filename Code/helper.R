345aa8f0f007bb3ebb86c391c4100de5582e9572
Yang Xiang <65746683+yyyxiang@users.noreply.github.com>
2024-09-17 19:30:43 -0400

    Add files via upload

---
+++ Code/helper.R
@@ -0,0 +1,362 @@
+bonus_model_predictions <- function(data) {
+  
+  data$force <- data$strength*data$effort/100
+  data$total_force <- data$s1*data$e1/100 + data$s2*data$e2/100  
+  df <- data %>% filter(subject == 1) %>% mutate(subject = NULL)
+  
+  df_fail <- df %>% filter(condition == 'fail')
+  df_fail$probability <- apply(array(1:dim(df_fail)[1]), 1, function(i){
+    mean(apply(array(seq(from = 1, to = 100 - df_fail$effort[i], by = 1)), 1, function(x){
+      x/100*df_fail$strength[i] + df_fail$total_force[i] >= df_fail$box_weight[i]
+    }))
+  })
+  
+  df_lift <- df %>% filter(condition == 'lift')
+  df_lift$probability <- apply(array(1:dim(df_lift)[1]), 1, function(i){
+    mean(apply(array(seq(from = 1, to = df_lift$effort[i], by = 1)), 1, function(x){
+      df_lift$total_force[i] - x/100*df_lift$strength[i] < df_lift$box_weight[i]
+    }))
+  })
+  
+  df <- rbind(df_fail, df_lift)
+  
+  df_flipped <- df %>% 
+    mutate(contestant = 3-contestant) %>% 
+    mutate(resp_NFA = (1-probability)*10) %>% 
+    select(exp_index, scenario, contestant, resp_NFA)
+  df <- merge(df, df_flipped)
+  
+  model_predictions <- df %>% 
+    mutate(prediction_F = force,
+           prediction_S = ifelse(condition == 'fail', 10-strength, strength),
+           prediction_E = effort/10,
+           prediction_FA = ifelse(condition == 'fail', 10-probability*10, probability*10),
+           prediction_NFA = ifelse(condition == 'fail', 10-resp_NFA, resp_NFA),
+           prediction_BA = (prediction_FA + prediction_NFA)/2,
+           prediction_EBA = (prediction_E + prediction_BA)/2)
+  model_predictions <- arrange(model_predictions, exp_index, scenario)
+  model_predictions <- model_predictions[, c('exp_index','condition','scenario','box_weight','contestant',
+                                             'strength','effort','s1','s2','e1','e2',
+                                             'prediction_E','prediction_S','prediction_F',
+                                             'prediction_BA','prediction_FA','prediction_NFA',
+                                             'prediction_EBA')]
+  
+  return(model_predictions)
+}
+
+resp_model_predictions <- function(data) {
+  
+  data$force <- data$strength*data$effort/100
+  data$total_force <- data$s1*data$e1/100 + data$s2*data$e2/100  
+  df <- data %>% filter(subject == 1) %>% mutate(subject = NULL)
+  
+  df_fail <- df %>% filter(condition == 'fail')
+  df_fail$probability <- apply(array(1:dim(df_fail)[1]), 1, function(i){
+    mean(apply(array(seq(from = 1, to = 100 - df_fail$effort[i], by = 1)), 1, function(x){
+      x/100*df_fail$strength[i] + df_fail$total_force[i] >= df_fail$box_weight[i]
+    }))
+  })
+  
+  df_lift <- df %>% filter(condition == 'lift')
+  df_lift$probability <- apply(array(1:dim(df_lift)[1]), 1, function(i){
+    mean(apply(array(seq(from = 1, to = df_lift$effort[i], by = 1)), 1, function(x){
+      df_lift$total_force[i] - x/100*df_lift$strength[i] < df_lift$box_weight[i]
+    }))
+  })
+  
+  df <- rbind(df_fail, df_lift)
+  
+  df_flipped <- df %>% 
+    mutate(contestant = 3-contestant) %>% 
+    mutate(resp_NFA = (1-probability)*10) %>% 
+    select(exp_index, scenario, contestant, resp_NFA)
+  df <- merge(df, df_flipped)
+  
+  model_predictions <- df %>% 
+    mutate(prediction_F = case_when(condition == 'fail' ~ 10-force,
+                                    condition == 'lift' ~ force),
+           prediction_S = strength,
+           prediction_E = case_when(condition == 'fail' ~ (100-effort)/10,
+                                    condition == 'lift' ~ effort/10),
+           prediction_FA = probability*10,
+           prediction_NFA = resp_NFA,
+           prediction_BA = (prediction_FA + prediction_NFA)/2,
+           prediction_EBA = (prediction_E + prediction_BA)/2)
+  model_predictions <- arrange(model_predictions, exp_index, scenario)
+  model_predictions <- model_predictions[, c('exp_index','condition','scenario','box_weight','contestant',
+                                             'strength','effort','s1','s2','e1','e2',
+                                             'prediction_E','prediction_S','prediction_F',
+                                             'prediction_BA','prediction_FA','prediction_NFA',
+                                             'prediction_EBA')]
+  
+  return(model_predictions)
+}
+
+plt_data <- function(dat, x, y) {
+  x <- enquo(x)
+  y <- enquo(y)
+  
+  return(
+    ggplot(dat, aes(!! x, !! y, group = scenario, color = condition)) +
+      stat_summary(fun.data = 'mean_cl_boot', geom = 'errorbar', width = .1) +
+      stat_summary(fun = 'mean', geom = 'point') +
+      stat_summary(fun = 'mean', geom = 'line') +
+      scale_color_manual(name = NULL, labels = c('Fail', 'Lift'),
+                         values = c('#C77CFF', '#00BAE0'),
+                         limits = c('fail', 'lift')) +
+      coord_cartesian(ylim = c(0, 10)) +
+      theme_bw()
+  )
+}
+
+line_plots <- function(dat, idx, y_label, title, ...) {
+  
+  subplot_a1 <- plt_data(dat %>% filter(exp_index == idx & s1==s2), contestant, ...) +
+    facet_wrap(~ type, strip.position ='right')
+  
+  subplot_b1 <- plt_data(dat %>% filter(exp_index == idx & e1==e2), contestant, ...) +
+    facet_wrap(~ type, strip.position ='right')
+  
+  subplot_c1 <- plt_data(dat %>% filter(exp_index == idx & (s1 != s2 & e1 != e2)), contestant, ...) +
+    facet_wrap(~ type, strip.position ='right')
+  
+  if (idx != 'resp2a' & idx != 'resp2b') {
+    return(
+      subplot_a1 + scale_x_discrete(labels=c("1" = "Less effort", "2" = "More effort")) +
+        labs(x = NULL, y = y_label, title = title) +
+        theme(legend.position = 'none', 
+              plot.title = element_text(hjust = 0.5),
+              strip.background = element_blank(),
+              strip.text = element_blank()) +
+        
+        subplot_b1 + scale_x_discrete(labels=c("1" = "Less strength", "2" = "More strength")) +
+        labs(x = NULL, y = y_label) +
+        theme(legend.position = 'none',
+              strip.background = element_blank(),
+              strip.text = element_blank()) +
+        
+        subplot_c1 + scale_x_discrete(labels=c("1" = "Less strength/\nmore effort", "2" = "More strength/\nless effort")) +
+        labs(x = 'Contestant', y = y_label) +
+        theme(legend.position = 'none',
+              strip.background = element_blank(),
+              strip.text = element_blank()) +
+        
+        plot_layout(nrow = 3)
+    )
+  } else if (idx == 'resp2a') {
+    return(
+      subplot_a1 + scale_x_discrete(labels=c("1" = "Less effort", "2" = "More effort")) +
+        labs(x = NULL, y = y_label, title = title) +
+        theme(legend.position = 'none', 
+              plot.title = element_text(hjust = 0.5)) +
+        
+        subplot_b1 + scale_x_discrete(labels=c("1" = "Less strength", "2" = "More strength")) +
+        labs(x = NULL, y = y_label) +
+        
+        subplot_c1 + scale_x_discrete(labels=c("1" = "Less strength/\nmore effort", "2" = "More strength/\nless effort")) +
+        labs(x = 'Contestant', y = y_label) +
+        theme(legend.position = 'none') +
+        
+        plot_layout(nrow = 3)
+    )
+  } else {
+    return(
+      subplot_a1 + scale_x_discrete(labels=c("1" = "Less effort", "2" = "More effort")) +
+        labs(x = NULL, y = y_label, title = title) +
+        theme(legend.position = 'none', 
+              plot.title = element_text(hjust = 0.5)) +
+        
+        subplot_b1 + scale_x_discrete(labels=c("1" = "Less strength", "2" = "More strength")) +
+        labs(x = NULL, y = y_label) +
+        theme(legend.position = 'none') +
+        
+        subplot_c1 + scale_x_discrete(labels=c("1" = "Less strength/\nmore effort", "2" = "More strength/\nless effort")) +
+        labs(x = 'Contestant', y = y_label) +
+        theme(legend.position = 'none') +
+        
+        plot_layout(nrow = 3)
+    )
+  }
+}
+
+bic_plot <- function(dat) {
+  # scale model predictions
+  dat_predictions <- dat %>%
+    select(starts_with('prediction_'))
+  dat_predictions <- rbind(
+    as.data.frame(scale(dat_predictions[dat$exp_index == unique(dat$exp_index)[1],])),
+    as.data.frame(scale(dat_predictions[dat$exp_index == unique(dat$exp_index)[2],]))
+  )
+  dat <- dat %>% 
+    select(!starts_with('prediction_')) %>% 
+    cbind(dat_predictions)
+  
+  bic_table <- NULL
+  for (exp in unique(dat$exp_index)) {
+    F_model <- lmer(response ~ prediction_F + condition + (1 + condition|subject),
+                    data = dat %>% filter(exp_index == exp), 
+                    REML = F,
+                    control = lmerControl(optimizer = 'bobyqa'))
+    S_model <- lmer(response ~ prediction_S + condition + (1 + condition|subject),
+                    data = dat %>% filter(exp_index == exp), 
+                    REML = F,
+                    control = lmerControl(optimizer = 'bobyqa'))
+    E_model <- lmer(response ~ prediction_E + condition + (1 + condition|subject),
+                    data = dat %>% filter(exp_index == exp), 
+                    REML = F,
+                    control = lmerControl(optimizer = 'bobyqa'))
+    FA_model <- lmer(response ~ prediction_FA + condition + (1 + condition|subject),
+                     data = dat %>% filter(exp_index == exp), 
+                     REML = F,
+                     control = lmerControl(optimizer = 'bobyqa'))
+    NFA_model <- lmer(response ~ prediction_NFA + condition + (1 + condition|subject),
+                      data = dat %>% filter(exp_index == exp), 
+                      REML = F,
+                      control = lmerControl(optimizer = 'bobyqa'))
+    BA_model <- lmer(response ~ prediction_BA + condition + (1 + condition|subject),
+                     data = dat %>% filter(exp_index == exp), 
+                     REML = F,
+                     control = lmerControl(optimizer = 'bobyqa'))
+    EBA_model <- lmer(response ~ prediction_EBA + condition + (1 + condition|subject),
+                      data = dat %>% filter(exp_index == exp), 
+                      REML = F,
+                      control = lmerControl(optimizer = 'bobyqa'))
+    df <- BIC(F_model, S_model, E_model, FA_model, NFA_model, BA_model, EBA_model) %>% select(BIC)
+    df$model <- rownames(df)
+    rownames(df) <- NULL
+    df$exp_index <- exp
+    bic_table <- rbind(bic_table, df)
+  }
+  bic_table$model <- factor(bic_table$model, labels = c('F','S','E','FA','NFA','BA','EBA'), 
+                            levels = c('F_model','S_model','E_model','FA_model',
+                                       'NFA_model','BA_model',
+                                       'EBA_model'), ordered = T)
+  
+  return(
+    bic_table %>% 
+      ggplot(aes(model, BIC, color = model)) +
+      geom_point(size = 3) +
+      facet_wrap(~ exp_index, nrow = 2, scales = 'free_y',
+                 labeller = as_labeller(c(`1a`='Exp 1a Bonus', `1b`='Exp 1b Bonus', 
+                                          `resp2a`='Xiang et al. (2023a) Responsibility', `resp2b`='Xiang et al. (2023a) Responsibility',
+                                          `2a`='Exp 2a Bonus', `2b`='Exp 2b Bonus'))) +
+      theme_bw() +
+      labs(x = 'Model')
+  )
+}
+
+single_participant_bic_plot <- function(dat) {
+  full_df <- NULL
+  full_p <- NULL
+  full_ci <- NULL
+  for (exp in unique(dat$exp_index)) {
+    subset <- dat %>% filter(exp_index == exp)
+    df <- NULL
+    for (s in unique(subset$subject)) {
+      F_model <- lm(response ~ prediction_F + condition, data = subset %>% filter(subject == s))
+      S_model <- lm(response ~ prediction_S + condition, data = subset %>% filter(subject == s))
+      E_model <- lm(response ~ prediction_E + condition, data = subset %>% filter(subject == s))
+      FA_model <- lm(response ~ prediction_FA + condition, data = subset %>% filter(subject == s))
+      NFA_model <- lm(response ~ prediction_NFA + condition, data = subset %>% filter(subject == s))
+      BA_model <- lm(response ~ prediction_BA + condition, data = subset %>% filter(subject == s))
+      EBA_model <- lm(response ~ prediction_EBA + condition, data = subset %>% filter(subject == s))
+      bic_df <- BIC(E_model, S_model, F_model, BA_model, FA_model, NFA_model, EBA_model)
+      if (is.infinite(bic_df$BIC)[1] != T) { 
+        df <- rbind(df, data.frame(exp_index = exp, subject = s, lowest = row.names(bic_df[which(bic_df$BIC == min(bic_df$BIC)),])))
+      } else {
+        print(paste0('Excluded subject ', s, ' in Exp ', exp, ' due to lack of variation'))
+      }
+    }
+    counts <- data.frame(rbind(table(df$lowest)))
+    num_subject <- length(unique(df$subject))
+    p <- counts/num_subject
+    ci <- sqrt(p*(1-p)/num_subject)*1.96
+    p$exp_index <- exp
+    ci$exp_index <- exp
+    full_df <- dplyr::bind_rows(full_df, df)
+    full_p <- dplyr::bind_rows(full_p, p)
+    full_ci <- dplyr::bind_rows(full_ci, ci)
+  }
+  
+  full_p <- full_p %>% pivot_longer(cols = !exp_index, names_to = 'model', values_to = 'p', names_pattern = '(.*)_model')
+  full_ci <- full_ci %>% pivot_longer(cols = !exp_index, names_to = 'model', values_to = 'ci', names_pattern = '(.*)_model')
+  full_p <- merge(full_p, full_ci) %>% 
+    replace_na(list(p = 0, ci = 0))
+  full_p$model <- factor(full_p$model, levels = c('F','S','E','FA','NFA','BA','EBA'), ordered = T)
+  
+  return(
+    full_p %>% ggplot(aes(model, p, fill = model)) +
+      geom_col(position = 'dodge') +
+      geom_errorbar(aes(ymin = p-ci, ymax = p+ci), width = .1,
+                    position = position_dodge(.9)) +
+      coord_cartesian(ylim = c(NA,1)) +
+      facet_wrap(~ exp_index, nrow = 2,
+                 labeller = as_labeller(c(`1a`='Exp 1a Bonus', `1b`='Exp 1b Bonus', `resp2a`='Xiang et al. (2023a) Responsibility', `resp2b`='Xiang et al. (2023a) Responsibility',
+                                          `2a`='Exp 2a Bonus', `2b`='Exp 2b Bonus'))) +
+      theme_bw() +
+      scale_y_continuous(labels = scales::label_percent(accuracy = 1)) +
+      labs(x = 'Model', y = 'Proportion')
+  )
+}
+
+merge_long <- function(dat, model_predictions) {
+  dat_summary <- dat %>%
+    group_by(exp_index, condition, scenario, contestant) %>%
+    dplyr::summarize(avg_response = mean(response),
+                     uci_response = CI(response)[1],
+                     lci_response = CI(response)[3])
+  dat_merge <- merge(dat_summary, model_predictions)
+  dat_merge_long <- dat_merge %>% pivot_longer(cols = c('prediction_E', 'prediction_BA', 'prediction_EBA'), 
+                                               names_to = 'model',
+                                               values_to = 'prediction')
+  dat_merge_long$model <- substr(dat_merge_long$model, 12, nchar(dat_merge_long$model))
+  dat_merge_long$model <- factor(dat_merge_long$model, levels = c('E', 'BA', 'EBA', ordered = T))
+  return(dat_merge_long)
+}
+
+correlations <- function(dat_merge_long) {
+  text_labels <- data.frame(exp_index = sort(rep(unique(dat_merge_long$exp_index),3)), model = rep(unique(dat_merge_long$model),2))
+  for (exp in unique(dat_merge_long$exp_index)) {
+    for (m in unique(dat_merge_long$model)) {
+      cor_both <- cor(dat_merge_long$avg_response[dat_merge_long$exp_index == exp & dat_merge_long$model == m], dat_merge_long$prediction[dat_merge_long$exp_index == exp & dat_merge_long$model == m], method = 'pearson', use = 'complete.obs')
+      cor_both <- paste0('r = ', format(round(cor_both, 2), nsmall = 2))
+      cor_fail <- cor(dat_merge_long$avg_response[dat_merge_long$condition == 'fail' & dat_merge_long$exp_index == exp & dat_merge_long$model == m], dat_merge_long$prediction[dat_merge_long$condition == 'fail' & dat_merge_long$exp_index == exp & dat_merge_long$model == m], method = 'pearson', use = 'complete.obs')
+      cor_fail <- paste0('r_fail = ', format(round(cor_fail, 2), nsmall = 2))
+      cor_lift <- cor(dat_merge_long$avg_response[dat_merge_long$condition == 'lift' & dat_merge_long$exp_index == exp & dat_merge_long$model == m], dat_merge_long$prediction[dat_merge_long$condition == 'lift' & dat_merge_long$exp_index == exp & dat_merge_long$model == m], method = 'pearson', use = 'complete.obs')
+      cor_lift <- paste0('r_lift = ', format(round(cor_lift, 2), nsmall = 2))
+      # Note: use cor.test() to get p-values
+      text_labels$text[text_labels$exp_index == exp & text_labels$model == m] <- paste0(cor_both, sep = '\n', cor_fail, sep = '\n', cor_lift)
+      text_labels$x[text_labels$exp_index == exp & text_labels$model == m] = 8
+      text_labels$y[text_labels$exp_index == exp & text_labels$model == m] = 1.5
+    }
+  }
+  return(text_labels)
+}
+
+cor_plt <- function(data, x, y, ymin, ymax, text_data) {
+  x <- enquo(x)
+  y <- enquo(y)
+  ymin <- enquo(ymin)
+  ymax <- enquo(ymax)
+  
+  return(
+    ggplot(data, aes(!! x, !! y, color = condition)) +
+      geom_errorbar(aes(ymin = !! ymin, ymax = !! ymax, width = 0)) +
+      geom_point() +
+      geom_abline(intercept = 0, slope = 1, lty = 3) +
+      scale_color_manual(name = NULL, labels = c('Fail', 'Lift'),
+                         values = c('#C77CFF', '#00BAE0'),
+                         limits = c('fail', 'lift')) +
+      scale_x_continuous(limits = c(0, 10), breaks = c(0,2.5,5,7.5,10)) +
+      scale_y_continuous(limits = c(0,10), breaks = c(0,2.5,5,7.5,10)) +
+      coord_fixed() +
+      facet_wrap(~ model, nrow = 3, strip.position = 'right',
+                 labeller = as_labeller(c(`E` = 'Effort', `BA` = 'BA counterfactual', `EBA` = 'Ensemble'))) +
+      theme_bw() +
+      geom_text(data = text_data,
+                mapping = aes(label = text, x = x , y = y),
+                inherit.aes = F,
+                fontface='italic')
+    
+  )
+}
